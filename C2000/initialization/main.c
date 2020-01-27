dbacf7fa33b3c2463539da57a6801dde7481c29f
hakan saraç <hakansrc@msn.com>
2020-01-27 20:35:10 +0300

    updated some comments

--- C2000/initialization/main.c
+++ C2000/initialization/main.c
@@ -24,9 +24,9 @@ int main(void)
     ConfigCpuTimer(&CpuTimer0, 200, 1000000); //1 seconds
     ConfigCpuTimer(&CpuTimer1, 200, 1000000); //1 seconds
     ConfigCpuTimer(&CpuTimer2, 200, 1000000); //1 seconds
-    CpuTimer0Regs.TCR.all = 0x4000; // enable cpu timer interrupt and disable others
-    CpuTimer1Regs.TCR.all = 0x4000; // enable cpu timer interrupt and disable others
-    CpuTimer2Regs.TCR.all = 0x4000; // enable cpu timer interrupt and disable others
+    CpuTimer0Regs.TCR.all = 0x4000; // enable cpu timer interrupt
+    CpuTimer1Regs.TCR.all = 0x4000; // enable cpu timer interrupt
+    CpuTimer2Regs.TCR.all = 0x4000; // enable cpu timer interrupt
 
     while(1)
     {
