6eb10436886a58aeb5774ccef81ad72f8b77f04c
hakan saraç <hakansrc@msn.com>
2020-01-27 20:35:00 +0300

    timer interrupt creation part is  added

---
+++ C2000/timerinterrupt/main.c
@@ -0,0 +1,71 @@
+#include <F2837xD_Device.h>
+#include <F2837xD_Examples.h>
+
+
+__interrupt void cpu_timer0_isr(void);  /*prototype of the ISR functions*/
+__interrupt void cpu_timer1_isr(void);  /*prototype of the ISR functions*/
+__interrupt void cpu_timer2_isr(void);  /*prototype of the ISR functions*/
+/**
+ * main.c
+ */
+int main(void)
+{
+
+    InitSysCtrl();  /*initialize the peripheral clocks*/
+
+    InitPieCtrl();  /*initialize the PIE table (interrupt related)*/
+    IER = 0x0000;   /*clear the Interrupt Enable Register   (IER)*/
+    IFR = 0x0000;   /*clear the Interrupt Flag Register     (IFR)*/
+    InitPieVectTable();
+
+    EALLOW;
+    CpuSysRegs.PCLKCR0.bit.TBCLKSYNC = 0;   /*stop the TimeBase clock for later synchronization*/
+    CpuSysRegs.PCLKCR0.bit.GTBCLKSYNC = 0;  /*stop the global TimeBase clock for later synchronization*/
+    EDIS;
+
+    /*Initialize cpu timers*/
+    InitCpuTimers();
+    ConfigCpuTimer(&CpuTimer0, 200, 1000000); //1 seconds
+    ConfigCpuTimer(&CpuTimer1, 200, 1000000); //1 seconds
+    ConfigCpuTimer(&CpuTimer2, 200, 1000000); //1 seconds
+    CpuTimer0Regs.TCR.all = 0x4000; // enable cpu timer interrupt
+    CpuTimer1Regs.TCR.all = 0x4000; // enable cpu timer interrupt
+    CpuTimer2Regs.TCR.all = 0x4000; // enable cpu timer interrupt
+
+    EALLOW;
+    PieVectTable.TIMER0_INT = &cpu_timer0_isr;  /*specify the interrupt service routine (ISR) address to the PIE table*/
+    PieVectTable.TIMER1_INT = &cpu_timer1_isr;  /*specify the interrupt service routine (ISR) address to the PIE table*/
+    PieVectTable.TIMER2_INT = &cpu_timer2_isr;  /*specify the interrupt service routine (ISR) address to the PIE table*/
+    EDIS;
+    IER |= M_INT1;  /*Enable the PIE group of Cpu timer 0 interrupt*/
+    IER |= M_INT13; /*Enable the PIE group of Cpu timer 1 interrupt*/
+    IER |= M_INT14; /*Enable the PIE group of Cpu timer 2 interrupt*/
+    PieCtrlRegs.PIECTRL.bit.ENPIE = 1; // Enable the PIE block
+    PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
+    EINT;  // Enable Global interrupt INTM
+    ERTM;  // Enable Global realtime interrupt DBGM
+
+
+    while(1)
+    {
+
+    }
+	return 0;
+}
+__interrupt void cpu_timer0_isr(void)
+{
+    CpuTimer0.InterruptCount++;
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+}
+
+__interrupt void cpu_timer1_isr(void)
+{
+    CpuTimer1.InterruptCount++;
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+}
+
+__interrupt void cpu_timer2_isr(void)
+{
+    CpuTimer2.InterruptCount++;
+
+}
